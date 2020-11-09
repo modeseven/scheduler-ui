af7eb3629b17dddbb8ee29dcad5036d4d888d738
mbegley <mikeabegley@gmail.com>
2020-11-09 16:05:18 -0500

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 3:54
+          Learn React 4:35
         </a>
       </header>
     </div>
